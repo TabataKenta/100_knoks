9287e9e08375442d4be28465597177fe6dd8c9ce
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2025-11-05 15:09:28 +0900

    Update 09.py

--- cp01/code/09.py
+++ cp01/code/09.py
@@ -18,7 +18,7 @@ if tmp!="":
 result=""
 s=""
 for i in range(len(word)):
-    if len(word[i])<=4:
+    if len(word[i])>=4:
         result+=word[i]
         result+=" "
     else:
@@ -34,4 +34,5 @@ for i in range(len(word)):
 print(result)       
             
         
+
         
