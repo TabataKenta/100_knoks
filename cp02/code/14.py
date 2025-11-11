a37ce72c4b97ede73153b290a513c4e466b8f381
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2025-11-11 20:13:26 +0900

    Create 14.py

---
+++ cp02/code/14.py
@@ -0,0 +1,13 @@
+def head_file(N):
+    cnt = 0
+    result=[]
+    with open('popular-names.txt', 'r') as f:
+        for line in f:
+            cnt += 1
+            if cnt <= N:
+                result.append(line.split("\t")[0])
+            else:
+                break
+    return result
+
+print(head_file(10))
