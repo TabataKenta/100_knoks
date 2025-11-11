400d3b635df5493c28c7822fb12b4e623b38bc09
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2025-11-11 20:13:45 +0900

    Create 15.py

---
+++ cp02/code/15.py
@@ -0,0 +1,29 @@
+def count_line(filename):
+    cnt=0
+    with open(filename,'r') as f:
+        for line in f:
+            cnt+=1
+    return cnt
+
+def file_N_split(N):
+    total_line=count_line('popular-names.txt')
+    cnt_split=total_line//N
+    
+    text=[]
+    cnt=0
+    file_num=1
+    with open('popular-names.txt','r') as f:
+        for line in f:
+            cnt+=1
+            text.append(line)
+            if cnt==cnt_split:
+                with open(f'popular-names_{file_num}.txt','w') as f2:
+                    f2.write(''.join(text))
+                    text=[]
+                    file_num+=1
+                    cnt=0
+    if text:
+        with open(f'popular-names_{file_num}.txt','w') as f2:
+            f2.write(''.join(text))
+            
+            
