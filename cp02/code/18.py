7cfcc2c39092dcc97fa5020286fc4c6c057b7381
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2025-11-11 20:14:55 +0900

    Create 18.py

---
+++ cp02/code/18.py
@@ -0,0 +1,15 @@
+#名前とその出現頻度をnameという辞書に格納
+name={}
+seen=set()
+with open('popular-names.txt', 'r') as f:
+        for line in f:
+            name_line=line.split("\t")[0]
+            if name_line not in seen:
+                seen.add(name_line)
+                name[name_line]=1
+            else:
+                name[name_line]+=1
+
+#出現頻度を降順でソート
+sorrt_naame=sorted(name.items(),key=lambda x:x[1],reverse=True) 
+print(sorrt_naame)    
