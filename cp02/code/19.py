b06c588aa2e456c39a28baf659263cb2fbb14322
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2025-11-11 23:38:53 +0900

    Update 19.py

--- cp02/code/19.py
+++ cp02/code/19.py
@@ -3,7 +3,7 @@ from operator import itemgetter
 lst=[]
 with open('popular-names.txt', 'r') as f:
     for line in f:
-        lst.append(line.split())
+        lst.append(line.split("\t"))
         
 sorted_lst=sorted(lst,key=itemgetter(3),reverse=True)
 print(sorted_lst)
