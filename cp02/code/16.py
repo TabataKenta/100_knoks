aef5f33a95b487a34dcd2c7ecc55a1974c843d44
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2025-11-11 20:14:03 +0900

    Create 16.py

---
+++ cp02/code/16.py
@@ -0,0 +1,13 @@
+import random
+
+def shuffle_lines(filename):
+    with open(filename, 'r') as f:
+        lines=f.readlines()#行ごとに分割したリストを取得
+        
+    random.shuffle(lines)
+    
+    with open("shuffled-"+filename, 'w') as f2:
+        f2.writelines(lines)
+        
+shuffle_lines('popular-names.txt')
+            
