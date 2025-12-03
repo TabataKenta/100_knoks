8e8ed56a119556ddf4dd358d4ddf29c53600fbf1
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2025-12-03 13:52:37 +0900

    Create 49.py

---
+++ cp05/code/49.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+# 1. 環境設定の読み込み
+load_dotenv()
+api_key = os.getenv("GOOGLE_API_KEY")
+
+if not api_key:
+    print("APIキーが見つかりません。.envを確認してください。")
+    exit()
+
+# 2. APIキーを設定
+genai.configure(api_key=api_key)
+
+# 3. モデルの準備
+model = genai.GenerativeModel("models/gemini-2.5-flash")
+
+text = """
+吾輩は猫である。名前はまだ無い。
+
+どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にもだいぶ逢ったがこんな片輪には一度も出会わした事がない。のみならず顔の真中があまりに突起している。そうしてその穴の中から時々ぷうぷうと煙を吹く。どうも咽せぽくて実に弱った。これが人間の飲む煙草というものである事はようやくこの頃知った。
+"""
+# 4. トークン数のカウント
+try:
+    response = model.count_tokens(text)
+    
+    # 5. 結果の表示
+    print(f"トークン数: {response.total_tokens}")
+
+except Exception as e:
+    print(f"エラーが発生しました: {e}")
