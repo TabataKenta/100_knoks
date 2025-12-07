0934d4150d4d4bce4cbbe85c615183fc20f76012
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2025-12-08 02:29:07 +0900

    Modify output of most_similar for better readability
    Print the top 10 most similar words to 'United_States'.

--- cp06/code/52.py
+++ cp06/code/52.py
@@ -2,4 +2,7 @@ import gensim
 
 model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz', binary=True)
 
-print(model.most_similar('United_States'))
+result = model.most_similar('United_States')
+
+for i in range(10):
+    print(result[i])
