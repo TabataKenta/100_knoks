ff34f11afa0dbb495b8f3e9c30a04124868ba6a1
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2025-12-08 02:32:36 +0900

    Modify most_similar call and update print loop

--- cp06/code/52.py
+++ cp06/code/52.py
@@ -2,7 +2,7 @@ import gensim
 
 model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin.gz', binary=True)
 
-result = model.most_similar('United_States')
+result = model.most_similar('United_States', topn=10)
 
-for i in range(10):
-    print(result[i])
+for word in result:
+    print(word)
