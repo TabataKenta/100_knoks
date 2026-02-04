c2f57ed2c6c7a668c262e8246f88dbac213f8efe
TabataKenta <k520288z@mails.cc.ehime-u.ac.jp>
2026-02-04 14:16:29 +0900

    Add mask filling function using BERT model

---
+++ cp09/code/p81.py
@@ -0,0 +1,11 @@
+from transformers import pipeline
+
+def top_mask_filler(text):
+    mask_filler = pipeline("fill-mask", model="bert-base-uncased")
+    results = mask_filler(text)
+    return results[0]['token_str']
+
+if __name__ == "__main__":
+    text = "The movie was full of [MASK]."
+    predicted_token = top_mask_filler(text)
+    print(f"Predicted token: {predicted_token}")
